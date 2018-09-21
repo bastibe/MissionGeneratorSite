6d171e1569da97344a90f6313961bec48eec0763
Bastian Bechtold <basti@bastibe.de>
2018-09-21 14:58:30 +0200

    adds upload script

---
+++ upload.bat
@@ -0,0 +1,2 @@
+scp debriefing.png "mission briefing.png" "mission list.png" "pilot profile.png" preferences.png "special missions.png" OpenSans-Regular.woff2 OpenSans-CondLight.woff2 index.html style.css msg@msg.uber.space:/home/msg/html/
+pause
