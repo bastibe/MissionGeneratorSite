0e0c14347ae90139673d5e15c089407fcab41f59
Bastian Bechtold <basti@bastibe.de>
2018-09-22 13:21:13 +0200

    works on Windows

--- upload.bat
+++ upload.bat
@@ -1,2 +1,2 @@
-scp debriefing.png "mission briefing.png" "mission list.png" "pilot profile.png" preferences.png "special missions.png" OpenSans-Regular.woff2 OpenSans-CondLight.woff2 index.html style.css msg@msg.uber.space:/home/msg/html/
+scp debriefing.png "mission briefing.png" "mission list.png" "pilot profile.png" preferences.png "special missions.png" face.jpg OpenSans-Regular.woff2 OpenSans-CondLight.woff2 index.html about.html news0.html news.xml privacy.html style.css msg@msg.uber.space:/home/msg/html/
 pause
